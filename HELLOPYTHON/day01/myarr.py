7308d085a1dd3ef26336602296042ce17029505d
JeonghoonWon <expedition1205@gmail.com>
2021-06-29 16:49:02 +0900

    python

--- HELLOPYTHON/day01/myarr.py
+++ HELLOPYTHON/day01/myarr.py
@@ -6,3 +6,15 @@ a.insert(len(a), "3")   #a.insert(index, object) ==> index : 배열의 위치 / 
 print(len(a))
 
 print(a)
+
+
+
+
+
+lol = [[1,2,3], [4,5], [6,7,8,9]]
+print(lol[0])
+print(lol[2][1])
+for sub in lol:
+    for item in sub:
+       print(item, end=" ")
+    print()
